3d69f85a1d735d35f12cc0322eccce9b99beb097
Albert <93777546+Neo802@users.noreply.github.com>
2022-11-25 22:31:47 +0200

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -8,8 +8,3 @@ int main()
 	init2();
 	return 0;
 }
-
-// Task: Implementation of linkedlist with the
-// most useful methods(add, remove, etc)
-// 1. First implm. must use friend classes and functions
-// 2. Second one must use nested classes.
